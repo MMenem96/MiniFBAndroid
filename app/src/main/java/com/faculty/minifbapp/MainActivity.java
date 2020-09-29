495abfd87c6ff86c881fc5b33fee8fcd9e585040
MMenem96 <mmoniemfahmy@gmail.com>
2020-09-29 13:46:18 +0200

    Implementation of MainActivity Logic,PostDetailsActivity Logic

--- app/src/main/java/com/faculty/minifbapp/MainActivity.java
+++ app/src/main/java/com/faculty/minifbapp/MainActivity.java
@@ -8,23 +8,30 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tvWelcome;
     private String email, name;
     private Button btnLogout;
+    private RecyclerView rvPosts;
+    private List<Post> posts = new ArrayList<>();
+    private PostsAdapter postsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        getUserDetailsFromCache();
+//        getUserDetailsFromCache();
         populateViews();
     }
 
     private void populateViews() {
-        tvWelcome.setText("Welcome " + name + "\n" + "Your email is " + email);
+//        tvWelcome.setText("Welcome " + name + "\n" + "Your email is " + email);
     }
 
     private void getUserDetailsFromCache() {
@@ -37,9 +44,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initViews() {
-        btnLogout = findViewById(R.id.btn_logout);
-        btnLogout.setOnClickListener(this);
-        tvWelcome = findViewById(R.id.tv_welcome);
+        rvPosts = findViewById(R.id.rv_posts);
+        posts.add(new Post(1, "HBD Ya Moniem", "29/9/2020", new User("Sheko")));
+        posts.add(new Post(2, "Our Second Post", "26/9/2020", new User("Youssef")));
+        posts.add(new Post(3, "Our Third Post", "25/9/2020", new User("Ebrahim")));
+        posts.add(new Post(4, "Our Fourth Post", "24/9/2020", new User("Mohamed")));
+        posts.add(new Post(5, "Our Fifth Post", "23/9/2020", new User("Waleed")));
+        posts.add(new Post(6, "Our 6th Post", "23/9/2020", new User("Nour")));
+        posts.add(new Post(7, "Our 7th Post", "23/9/2020", new User("Shourok")));
+        posts.add(new Post(8, "Our 7th Post", "23/9/2020", new User("Moniem")));
+        postsAdapter = new PostsAdapter(posts);
+        rvPosts.setAdapter(postsAdapter);
+
+//        btnLogout = findViewById(R.id.btn_logout);
+//        btnLogout.setOnClickListener(this);
+//        tvWelcome = findViewById(R.id.tv_welcome);
     }
 
     private void openLoginActivity() {
@@ -52,12 +71,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_logout) {//Clear Shared Prefs
-            //Open LoginActivity
-            //Finish MainActivity
-            clearCache();
-            openLoginActivity();
-            finish();
-        }
+//        if (v.getId() == R.id.btn_logout) {//Clear Shared Prefs
+//            //Open LoginActivity
+//            //Finish MainActivity
+//            clearCache();
+//            openLoginActivity();
+//            finish();
+//        }
     }
 }
---
+++ app/src/main/java/com/faculty/minifbapp/User.java
@@ -0,0 +1,36 @@
+package com.faculty.minifbapp;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
